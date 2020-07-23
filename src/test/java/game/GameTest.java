430933cfb8d62c9256544c093b3493f39d94f43d
LIANGFR2 <372334264@qq.com>
2020-07-23 19:30:43 +0800

    [guess-number][Front] test:should_return_0A0B_when_guess_given_answer_1234_and_guess_numbers_5678

--- src/test/java/game/GameTest.java
+++ src/test/java/game/GameTest.java
@@ -26,4 +26,25 @@ public class GameTest {
         //then
         assertEquals("4A0B", result);
     }
+
+    /*
+    输入：
+    answer:[int]
+    guessNumbers:[int]
+    输出:
+    0A0B
+     */
+    @Test
+    void should_return_0A0B_when_guess_given_answer_1234_and_guess_numbers_5678() {
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        int[] answer = {1,2,3,4};
+        int[] guessNumbers = {5,6,7,8};
+
+        //when
+        String result = guessNumber.guess(answer, guessNumbers);
+
+        //then
+        assertEquals("0A0B", result);
+    }
 }
