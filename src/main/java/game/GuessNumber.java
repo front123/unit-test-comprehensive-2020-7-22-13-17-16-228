ea747b3e5bb081ff79459a85e134a46ed800cf28
LIANGFR2 <372334264@qq.com>
2020-07-23 19:34:16 +0800

    [guess-number][Front] feature:pass should_return_0A0B_when_guess_given_answer_1234_and_guess_numbers_5678

--- src/main/java/game/GuessNumber.java
+++ src/main/java/game/GuessNumber.java
@@ -2,7 +2,17 @@ package game;
 
 public class GuessNumber {
 
-    public String guess(int[] answer, int[] guessNumbers){
-        return "4A0B";
+    public String guess(int[] answer, int[] guessNumbers) {
+        boolean isAllRight = true;
+        for (int i = 0; i < answer.length; i++) {
+            if(answer[i]!=guessNumbers[i]) {
+                isAllRight = false;
+                break;
+            }
+        }
+        if (isAllRight){
+            return "4A0B";
+        }
+        return "0A0B";
     }
 }
