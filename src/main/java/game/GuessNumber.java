3bbf32b5c8afc77ec24118437e6154a5cd10839c
LIANGFR2 <372334264@qq.com>
2020-07-23 20:05:32 +0800

    [guess-number][Front] feature:pass should_return_0A4B_when_guess_given_answer_1234_and_guess_numbers_4321

--- src/main/java/game/GuessNumber.java
+++ src/main/java/game/GuessNumber.java
@@ -1,15 +1,25 @@
 package game;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GuessNumber {
 
     public String guess(int[] answer, int[] guessNumbers) {
 //        boolean isAllRight = true;
         int countNumberInAnswerAndPositionRight = 0;
-        for (int i = 0; i < answer.length; i++) {
-            if(answer[i] == guessNumbers[i]){
-                countNumberInAnswerAndPositionRight++;
+        int countNumberInAnswerButWrongPosition = 0;
+        for (int i = 0; i < guessNumbers.length; i++) {
+            for (int j=0; j < answer.length; j++){
+                if (guessNumbers[i] == answer[j] && i==j){
+                    countNumberInAnswerAndPositionRight ++;
+                }
+                if(guessNumbers[i] == answer[j] && i!=j){
+                    countNumberInAnswerButWrongPosition++;
+                }
             }
         }
-        return countNumberInAnswerAndPositionRight +"A0B";
+
+        return countNumberInAnswerAndPositionRight +"A"+countNumberInAnswerButWrongPosition+"B";
     }
 }
