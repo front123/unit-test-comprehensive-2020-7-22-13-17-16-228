de6838bad89c4d3b221ab608728c71fe36d382f2
LIANGFR2 <372334264@qq.com>
2020-07-23 21:00:52 +0800

    [guess-number][Front] test: should_return_false_when_check_is_valid_guess_numbers_given_1232

--- src/test/java/game/GameTest.java
+++ src/test/java/game/GameTest.java
@@ -160,4 +160,15 @@ public class GameTest {
         //then
         assertFalse(result);
     }
+
+    @Test
+    void should_return_false_when_check_is_valid_guess_numbers_given_1232() {
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        int[] guessNumbers = {1,2,3,2};
+        //when
+        boolean result = guessNumber.isValidGuessNumbers(guessNumbers);
+        //then
+        assertFalse(result);
+    }
 }
