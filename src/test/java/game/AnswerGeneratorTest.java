043625ebcff4c82a08ce004d04d9015605d5c89d
LIANGFR2 <372334264@qq.com>
2020-07-26 16:52:14 +0800

    [guess-number][Front] feature:pass should_random_answer_numbers_length_is_4_when_generate_numbers_given

--- src/test/java/game/AnswerGeneratorTest.java
+++ src/test/java/game/AnswerGeneratorTest.java
@@ -3,23 +3,19 @@ package game;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class AnswerGeneratorTest {
 
     private final GuessNumberGame guessNumberGame = new GuessNumberGame();
 
     @Test
-    void should_return_true_when_is_valid_answer_numbers_given_random_answer_numbers() {
+    void should_random_answer_numbers_length_is_4_when_generate_numbers_given() {
         //given
         AnswerGenerator answerGenerator = new AnswerGenerator();
-        int[] answerNumbers = answerGenerator.generate();
 
         //when
-        boolean result = guessNumberGame.isValidNumbers(answerNumbers);
+        int[] answerNumbers = answerGenerator.generate();
 
         //then
-        assertTrue(result);
+        Assertions.assertEquals(4, answerNumbers.length);
     }
 }
