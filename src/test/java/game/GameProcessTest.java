0e44a14342ad0879f6fb06e30da6fa6d5d69ecc5
LIANGFR2 <372334264@qq.com>
2020-07-27 23:53:18 +0800

    [guess-number][Front] refactor:use mock inputValidator to set isValidNumber action return false

--- src/test/java/game/GameProcessTest.java
+++ src/test/java/game/GameProcessTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 
 public class GameProcessTest {
     private final INumberGenerator answerGenerator = mock(INumberGenerator.class);
+    private final InputValidator inputValidator = mock(InputValidator.class);
     private final GuessNumberGame guessNumberGame = mock(GuessNumberGame.class);
     @Test
     void should_output_wrong_message_when_guess_given_1232() {
@@ -15,6 +16,7 @@ public class GameProcessTest {
         int[] answer = {1, 2, 3, 4};
         int[] guessNumbers = {1, 2, 3, 2};
         when(answerGenerator.generate()).thenReturn(answer);
+        when(inputValidator.isValidNumbers(guessNumbers)).thenReturn(false);
         GameProcess gameProcess = new GameProcess();
 
         //when
