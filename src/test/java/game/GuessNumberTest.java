d83079b144ec3fc2c17ad03e0508cf883007a537
LIANGFR2 <372334264@qq.com>
2020-07-27 23:03:52 +0800

    [guess-number][Front] refactor:rename GameTest to GuessNumberTest

--- src/test/java/game/GuessNumberTest.java
+++ src/test/java/game/GuessNumberTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class GameTest {
+public class GuessNumberTest {
 
     private final INumberGenerator answerGenerator = mock(INumberGenerator.class);
 
