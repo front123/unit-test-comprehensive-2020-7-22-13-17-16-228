ffcafe21362e43d193a484f67b532aba8b570df8
LIANGFR2 <372334264@qq.com>
2020-07-27 21:04:12 +0800

    [guess-number][Front] test pass:should_distinct_numbers_amount_equal_answerNumbers_size_when_generate_numbers_given

--- src/test/java/game/AnswerGeneratorTest.java
+++ src/test/java/game/AnswerGeneratorTest.java
@@ -3,6 +3,9 @@ package game;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class AnswerGeneratorTest {
 
     private final GuessNumberGame guessNumberGame = new GuessNumberGame();
@@ -17,5 +20,22 @@ public class AnswerGeneratorTest {
 
         //then
         Assertions.assertEquals(4, answerNumbers.length);
+    }
+
+    @Test
+    void should_distinct_numbers_amount_equal_answerNumbers_size_when_generate_numbers_given() {
+        //given
+        AnswerGenerator answerGenerator = new AnswerGenerator();
+
+        //when
+        int[] answerNumbers = answerGenerator.generate();
+        long distinctAmount = Arrays.stream(answerNumbers).boxed()
+                .collect(Collectors.toList())
+                .stream()
+                .distinct()
+                .count();
+
+        //then
+        Assertions.assertEquals(answerNumbers.length, distinctAmount);
     }
 }
