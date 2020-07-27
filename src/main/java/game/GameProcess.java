6e9f93a8949422f41c15bc08dff5c0668b915964
LIANGFR2 <372334264@qq.com>
2020-07-28 00:14:21 +0800

    [guess-number][Front] refactor:add timesLimiter and increase guess times in GameProcess.play

--- src/main/java/game/GameProcess.java
+++ src/main/java/game/GameProcess.java
@@ -1,12 +1,20 @@
 package game;
 
 public class GameProcess {
-    private final InputValidator inputValidator = new InputValidator();
-    private GuessNumberGame guessNumberGame;
-    public String play(int[] guessNumbers) {
+    private final InputValidator inputValidator;
+    private final GuessNumberGame guessNumberGame;
+    private final TimesLimiter timesLimiter;
+    public GameProcess() {
+        this.guessNumberGame = new GuessNumberGame();
+        this.inputValidator = new InputValidator();
+        this.timesLimiter = new TimesLimiter(6);
+    }
+
+    public String play(int[] guessNumbers) throws TimesOutException {
         if (!inputValidator.isValidNumbers(guessNumbers)) {
             return "Wrong Input，Input again";
         }
+        timesLimiter.increase();
         return guessNumberGame.guess(guessNumbers);
     }
 }
