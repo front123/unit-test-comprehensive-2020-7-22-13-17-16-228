a4e2e83473df6c2b7683c869ec4c80f2fd6bf16f
LIANGFR2 <372334264@qq.com>
2020-07-28 00:26:45 +0800

    [guess-number][Front] test:should_return_win_message_when_play_given_numbers_1234_and_answer_1234

--- src/test/java/game/GameProcessTest.java
+++ src/test/java/game/GameProcessTest.java
@@ -10,18 +10,34 @@ public class GameProcessTest {
     private final INumberGenerator answerGenerator = mock(INumberGenerator.class);
     private final InputValidator inputValidator = mock(InputValidator.class);
     private final GuessNumberGame guessNumberGame = mock(GuessNumberGame.class);
+    private final TimesLimiter timesLimiter = mock(TimesLimiter.class);
     @Test
-    void should_output_wrong_message_when_play_given_guess_numbers_1232() {
+    void should_output_wrong_message_when_play_given_guess_numbers_1232() throws TimesOutException {
         //given
-        int[] answer = {1, 2, 3, 4};
+        int[] answer = {1,2,3,4};
         int[] guessNumbers = {1, 2, 3, 2};
-        when(answerGenerator.generate()).thenReturn(answer);
+        when(guessNumberGame.getAnswer()).thenReturn(answer);
         when(inputValidator.isValidNumbers(guessNumbers)).thenReturn(false);
-        GameProcess gameProcess = new GameProcess();
+        GameProcess gameProcess = new GameProcess(guessNumberGame, inputValidator, timesLimiter);
 
         //when
         String result = gameProcess.play(guessNumbers);
         //then
         assertEquals("Wrong Input，Input again", result);
+    }
+
+    @Test
+    void should_return_win_message_when_play_given_numbers_1234_and_answer_1234() throws TimesOutException {
+        int[] answer = {1, 2, 3, 4};
+        int[] guessNumbers = {1, 2, 3, 4};
+        when(guessNumberGame.getAnswer()).thenReturn(answer);
+        when(inputValidator.isValidNumbers(guessNumbers)).thenReturn(true);
+        when(guessNumberGame.guess(guessNumbers)).thenReturn("4A0B");
+        GameProcess gameProcess = new GameProcess(guessNumberGame, inputValidator, timesLimiter);
+
+        //when
+        String result = gameProcess.play(guessNumbers);
+        //then
+        assertEquals("Win", result);
     }
 }
