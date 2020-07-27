26a985bd687295b2f4ef5740b15b076cdbeb8aa1
LIANGFR2 <372334264@qq.com>
2020-07-28 00:23:32 +0800

    [guess-number][Front] refactor:add answerGenerator and create a constructor with parameter guessNumberGame,inputValidator,timesLimiter for test

--- src/main/java/game/GameProcess.java
+++ src/main/java/game/GameProcess.java
@@ -4,10 +4,18 @@ public class GameProcess {
     private final InputValidator inputValidator;
     private final GuessNumberGame guessNumberGame;
     private final TimesLimiter timesLimiter;
+    private final AnswerGenerator answerGenerator;
     public GameProcess() {
-        this.guessNumberGame = new GuessNumberGame();
+        this.answerGenerator = new AnswerGenerator();
+        this.guessNumberGame = new GuessNumberGame(answerGenerator);
         this.inputValidator = new InputValidator();
         this.timesLimiter = new TimesLimiter(6);
+    }
+    public GameProcess(GuessNumberGame guessNumberGame, InputValidator inputValidator, TimesLimiter timesLimiter) {
+        this.guessNumberGame = guessNumberGame;
+        this.inputValidator = inputValidator;
+        this.timesLimiter = timesLimiter;
+        this.answerGenerator = new AnswerGenerator();
     }
 
     public String play(int[] guessNumbers) throws TimesOutException {
